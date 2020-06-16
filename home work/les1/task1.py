9559b2c57e45077a23b2437eef5d464e8c44a44c
Notpil33 <stasinus30@gmail.com>
2020-06-17 02:30:56 +0400

    Домашнее задание к первому уроку.

---
+++ home work/les1/task1.py
@@ -0,0 +1,7 @@
+name = input("Ваше имя:")
+age = input("Сколько вам лет:")
+city = input("Из какого вы города:")
+info = input("Ваше увлечение:")
+number = input("Ваш номер телефона:")
+
+print(name, age, city, info)
